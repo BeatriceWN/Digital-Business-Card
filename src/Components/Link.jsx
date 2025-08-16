117d777f85cd90314a69d8cf170edde4dba85a79
Beatrice Wambui <bettyneema2@gmail.com>
2025-08-16 22:06:14 +0300

    Links component initial commit

---
+++ src/Components/Link.jsx
@@ -0,0 +1,11 @@
+import { FaPhone, FaEnvelope, FaGithub, FaLinkedin } from "react-icons/fa";
+
+export default function Link() {
+    return (
+        <>
+            <h2>Links</h2>
+            <p><FaGithub />github.com/BeatriceWN</p>
+            <p><FaLinkedin />linkedin.com/in/beatricewambui</p>
+        </>
+    )
+}
