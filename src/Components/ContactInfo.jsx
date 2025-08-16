1c0ae785d33d651c19c197844350e9f39f553dbd
Beatrice Wambui <bettyneema2@gmail.com>
2025-08-16 22:01:56 +0300

    Initial commit after creating components and initializing repo

---
+++ src/Components/ContactInfo.jsx
@@ -0,0 +1,13 @@
+import { FaPhone, FaEnvelope, FaGithub, FaLinkedin } from "react-icons/fa";
+
+export default function ContactInfo() {
+    return (
+        <>
+            <h2>Contact</h2>
+            <p><FaPhone />+254 728 459 520</p>
+            <p><FaEnvelope />bettyneema2@gmail.com</p>
+            <p><FaGithub />github.com/BeatriceWN</p>
+            <p><FaLinkedin />linkedin.com/in/beatricewambui</p>
+        </>
+    )
+}
