21a631acd8642cf5f8d1ae9a98d7f5a8578318b4
Beatrice Wambui <bettyneema2@gmail.com>
2025-08-17 07:24:33 +0300

    Added class names for styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,17 @@
+import ContactInfo from "./Components/ContactInfo";
+import Link from "./Components/Link";
+import Navbar from "./Components/Navbar";
+import ProfileCard from "./Components/ProfileCard";
+import QRCodeSection from "./Components/QRCodeSection";
 
+export default function App() {
+  return (
+    <>
+    <ProfileCard />
+    <ContactInfo />
+    <Link />
+    <QRCodeSection />
+    <Navbar />
+    </>
+  )
+}
