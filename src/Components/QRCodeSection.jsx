4ccbca294220915aaa585e7df1753553f8fa913b
Beatrice Wambui <bettyneema2@gmail.com>
2025-08-17 07:29:07 +0300

    Added class names in qr code section component for styling

--- src/Components/QRCodeSection.jsx
+++ src/Components/QRCodeSection.jsx
@@ -2,10 +2,9 @@ import { QRCodeCanvas } from "qrcode.react";
 
 export default function QRCodeSection() {
     return (
-        <div>
+        <div className="qr-card">
            <h3>Share</h3>
-           <QRCodeCanvas value="https://github.com/BeatriceWN/Digital-Business-Card" />
-           <p>Show QR Code</p>
+           <QRCodeCanvas value="https://github.com/BeatriceWN/Digital-Business-Card" size={200} />
         </div>
     )
 }
